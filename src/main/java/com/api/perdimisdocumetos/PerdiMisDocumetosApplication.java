19086b602b4deaf5ac22f0e0eaf351ce10ed9ed8
ingyonny90 <80722431+ingyonny90@users.noreply.github.com>
2021-05-07 13:03:37 -0500

    repositorio creado

---
+++ src/main/java/com/api/perdimisdocumetos/PerdiMisDocumetosApplication.java
@@ -0,0 +1,13 @@
+package com.api.perdimisdocumetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerdiMisDocumetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerdiMisDocumetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/perdimisdocumetos/PerdimisdocumetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.perdimisdocumetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerdimisdocumetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
